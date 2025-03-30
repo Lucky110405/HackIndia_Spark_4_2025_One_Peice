0eba047eb5aaf85f266f965f32144bf8f6d6a3f1
SinHunter07 <tanishkpandey36@gmail.com>
2025-03-31 03:42:43 +0530

    added utils

---
+++ src/utils/AsyncHandler.js
@@ -0,0 +1,8 @@
+const AyncHandler = (requestHandler) => {
+    return (req,res,next) => {
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=> next(err))
+    }
+} 
+
+export {AyncHandler}
+
