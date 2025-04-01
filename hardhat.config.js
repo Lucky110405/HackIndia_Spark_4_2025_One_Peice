9c245372563cc95017018a37a1c7148830cd4be6
Lucky110405 <mlucky110405@gmail.com>
2025-04-01 15:38:41 +0530

    solidity contract deployed on amoy testnet and verified by polygonscan

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomicfoundation/hardhat-verify");
 require("dotenv").config();
 
 module.exports = {
@@ -9,8 +10,24 @@ module.exports = {
       url: "http://127.0.0.1:8545"
     },
     amoy: {
-      url: `https://amoy.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts: [process.env.PRIVATE_KEY]
+      url: `https://polygon-amoy.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 80002
     }
+  },
+  etherscan: {
+    apiKey: {
+      amoy: process.env.POLYGONSCAN_API_KEY
+    },
+    customChains: [
+      {
+        network: "amoy",
+        chainId: 80002,
+        urls: {
+          apiURL: "https://api-amoy.polygonscan.com/api",
+          browserURL: "https://amoy.polygonscan.com"
+        }
+      }
+    ]
   }
 };
